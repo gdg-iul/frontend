833b9bc7aca206f26319a88ef45f5edf67d3e1af
Adnan <138090937+AdnanHusayn@users.noreply.github.com>
2025-10-07 17:50:53 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <div className='flex items-center justify-center bg-gray-800 min-h-screen'>
+          <div className='text-2xl text-white font-bold'>Hello, Tech Team 🧑🏻‍💻</div>
+        </div>
+    </>
+  )
+}
+
+export default App
